ab763e5641a5e55366a476ecc7c0b835ecd677d4
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-06-08 08:48:41 +0530

    Date integration

--- client/src/pages/Diary/Diary.jsx
+++ client/src/pages/Diary/Diary.jsx
@@ -1,13 +1,25 @@
+import { Button } from '@mui/material';
 import React from 'react'
 import { useDate } from '../../utils/hooks/useDate'
 
 export default function Diary() {
-  const date = useDate()[0];
+  const [date, setDate, reset, previous, next] = useDate();
 
   return (
     <div>
       hello diary
       <h2>Date is : {date}</h2>
+      <Button onClick = {() => {previous()}}>
+        Previous
+      </Button> 
+
+      <Button onClick = {() => {next()}}>
+        Next
+      </Button> 
+
+      <Button onClick = {() => {reset()}}>
+        Reset
+      </Button> 
     </div>
   )
 }
