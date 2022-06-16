b303fa768b10fb79d15e6249a1b9a70ccc13b9d9
Ishwarendra Jha <ishwarendra27@gmail.com>
2022-06-16 17:28:54 +0530

    readme and card fixes

--- client/src/pages/Favorite/Favorite.jsx
+++ client/src/pages/Favorite/Favorite.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import {Sidebar} from './../../components';
-import {Cards} from './../../elements'
+import React from "react";
+import { Sidebar } from "./../../components";
+import { Cards } from "./../../elements";
 
-import './Favorite.css';
+import "./Favorite.css";
 
 export default function Favorite() {
   return (
     <div>
-        <Sidebar />
+      <Sidebar />
 
-        <div className='favorite-container'>
-            <Cards date={"23 Sep, 2022"} time={"22:00:03"} title="Ishwar" 
-            content={`The oldest classical British and Latin writing had little or no space between words and could be written in boustrophedon (alternating directions). Over time, text direction (left to right) became standardized, and word dividers and terminal punctuation became common. The first way to divide sentences into groups was the original paragraphs, similar to an underscore at the beginning of the new group.[2] The Greek parágraphos evolved into the pilcrow (¶), which in English manuscripts in the Middle Ages can be seen inserted inline between sentences. The hedera leaf (e.g. ☙) has also been used in the same way..`} />
-        </div>
+      <div className="favorite-container">
+        <Cards
+          date={"23 Sep, 2022"}
+          time={"22:00:03"}
+          title="Ishwar"
+          content={`The oldest classical.`}
+        />
+      </div>
     </div>
-  )
+  );
 }
