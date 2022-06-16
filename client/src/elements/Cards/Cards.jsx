2dfe19020ac5ba4eb98354014c0c9218b2744706
Ishwarendra Jha <ishwarendra27@gmail.com>
2022-06-16 17:17:27 +0530

    show less button in content added:

--- client/src/elements/Cards/Cards.jsx
+++ client/src/elements/Cards/Cards.jsx
@@ -1,28 +1,29 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { Box } from "@mui/material";
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
   return <IconButton {...other} />;
 })(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
     duration: theme.transitions.duration.shortest,
   }),
 }));
 
-// Date, Time, Title, Content 
-export default function RecipeReviewCard({date, time, title, content}) {
+// Date, Time, Title, Content
+export default function RecipeReviewCard({ date, time, title, content }) {
   const [expanded, setExpanded] = React.useState(false);
 
   const handleExpandClick = () => {
@@ -30,12 +31,9 @@ export default function RecipeReviewCard({date, time, title, content}) {
   };
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardHeader
-        title={date}
-        subheader={`Last updated: ${time}`}
-      />
-      
+    <Card sx={{ maxWidth: "45%" }}>
+      <CardHeader title={date} subheader={`Last updated: ${time}`} />
+
       <CardContent>
         <Typography variant="h6" gutterbottom>
           {title}
@@ -57,7 +55,18 @@ export default function RecipeReviewCard({date, time, title, content}) {
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
           <Typography paragraph>
-            <hr />{content}
+            <hr />
+            {content}
+            <CardActions disableSpacing>
+              <ExpandMore
+                expand={expanded}
+                onClick={handleExpandClick}
+                aria-expanded={expanded}
+                aria-label="show less"
+              >
+                <ExpandMoreIcon />
+              </ExpandMore>
+            </CardActions>
           </Typography>
         </CardContent>
       </Collapse>
--- client/src/pages/Favorite/Favorite.jsx
+++ client/src/pages/Favorite/Favorite.jsx
@@ -10,8 +10,8 @@ export default function Favorite() {
         <Sidebar />
 
         <div className='favorite-container'>
-            <Cards date={"23 Sep, 2022"} time={"22:00:03"} title="Ishwar" content="Ishwarendra" />
-            
+            <Cards date={"23 Sep, 2022"} time={"22:00:03"} title="Ishwar" 
+            content={`The oldest classical British and Latin writing had little or no space between words and could be written in boustrophedon (alternating directions). Over time, text direction (left to right) became standardized, and word dividers and terminal punctuation became common. The first way to divide sentences into groups was the original paragraphs, similar to an underscore at the beginning of the new group.[2] The Greek parágraphos evolved into the pilcrow (¶), which in English manuscripts in the Middle Ages can be seen inserted inline between sentences. The hedera leaf (e.g. ☙) has also been used in the same way..`} />
         </div>
     </div>
   )
