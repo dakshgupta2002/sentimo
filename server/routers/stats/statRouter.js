9d192e55e8e9c47f32af63fbb80b9f56025c5d63
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-06-18 14:40:39 +0530

    wait for it :evil:

--- server/routers/stats/statRouter.js
+++ server/routers/stats/statRouter.js
@@ -51,47 +51,6 @@ statRouter.route("/note")
         });
     })
 
-//stat of a user of a particular date 
-statRouter.route("/date")
-    .post(async (req, res) => {
-        const text = req.body.text;
-        const date = req.body.date;
-        let emotion;
-
-        let data = encodeURI(text);
-        // spawn new child process to call the python script
-        const python = spawn('python', ['scripts/emotion.py', data]);
-
-        python.stdout.on('data', data => {
-            emotion = data.toString();
-        });
-
-        python.on('close', async () => {
-            var obj = "";
-            for (var i = 0; i < emotion.length; i++) {
-                if (emotion[i] === "'") {
-                    obj += '"';
-                } else obj += emotion[i];
-            }
-
-            obj = (JSON.parse(obj));
-            const newStat = await new Stat({
-                user: req.user._id,
-                emotion,
-                date
-            });
-
-            newStat.save().then(stat => {
-                res.status(201).json({ stat });
-            }).catch(() => {
-                res.status(400).json({ message: "Error generating stats" });
-            })
-        });
-
-        res.status(500).json({ "msg": "Error generating stats" });
-
-    })
-
 
 statRouter.route("/")
     .get((req, res) => {
