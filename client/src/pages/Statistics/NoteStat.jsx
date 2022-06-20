b905dd0e3b08f84390712f04e167125ba70cebbd
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-06-20 13:40:31 +0530

    Victory graph :tada:

--- client/src/pages/Statistics/NoteStat.jsx
+++ client/src/pages/Statistics/NoteStat.jsx
@@ -4,7 +4,7 @@ import { Sidebar } from "../../components";
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import { fetchNoteStats } from "../../utils/api/stats";
-import { VictoryPie } from "victory";
+import { VictoryPie, VictoryAxis, VictoryBar, VictoryChart } from "victory";
 import { Cards } from "../../elements";
 //fetch stats for particulary only this note Id
 //and stats should be converted to graphs and illustrations
@@ -15,6 +15,7 @@ export default function NoteStat() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [pieData, setPieData] = useState([]);
+  const [lineData, setLineData] = useState([{"x": "", "y": 0}]);
 
   useEffect(() => {
     const fetchStat = async () => {
@@ -42,7 +43,20 @@ export default function NoteStat() {
       if (emotion.Fear) data.push({ x: "Fear", y: Math.round(emotion?.Fear * 360) });
       setPieData(data);
     };
+
+    const refreshLineData = () => {
+      const data = [];
+      if (emotion.Sad) data.push({ x:1 , y: Math.round(emotion?.Sad * 100) });
+      if (emotion.Angry) data.push({ x: 2, y: Math.round(emotion?.Angry * 100) });
+      if (emotion.Surprise) data.push({ x: 3 , y: Math.round(emotion?.Surprise * 100) });
+      if (emotion.Fear) data.push({ x: 4, y: Math.round(emotion?.Fear * 100) });
+      if (emotion.Happy) data.push({ x:5 , y: Math.round(emotion?.Happy * 100) });
+      setLineData(data);
+    }
+    console.log(lineData);
+
     refreshPieData();
+    refreshLineData();
   }, [emotion]);
 
   return (
@@ -55,9 +69,30 @@ export default function NoteStat() {
         data={pieData}
         style={{ labels: { fontSize: 5, fontWeight: "bold" } }}
         labels={({ datum }) => `${datum.x}: ${datum.y}`}
-      />
-      {/* <h1>{title}</h1>
-      <h2>{content} </h2> */}
+      />  
+
+      <VictoryChart domainPadding={20} height={200}>
+        <VictoryAxis
+          tickValues={[1, 2, 3, 4, 5]}
+          tickFormat={["Sad", "Angry", "Surprise", "Fear", "Happy"]}
+        />
+        <VictoryAxis
+          height={300}
+          dependentAxis
+          tickFormat={(x) => (x)}
+        />
+        <VictoryBar
+          data={lineData}
+          x="x"
+          y="y"
+          animate={{
+            duration: 2000,
+            onLoad: { duration: 1000 }
+          }}
+        />
+      </VictoryChart>
+
+
       <Cards noteId={noteId} date={null} time={null} title={title} content={content} favourite={-1} protect={-1} />
       <span>
         <h4>Happy</h4>: {emotion.Happy}{" "}
