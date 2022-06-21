f3fd225116fb810028a7b113a21f598abd97df0c
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-06-21 19:09:41 +0530

    deploying code

--- server/index.js
+++ server/index.js
@@ -8,8 +8,6 @@ import notesRouter from './routers/notes/index.js';
 import statsRouter from './routers/stats/index.js';
 import profileRouter from './routers/profile/index.js';
 import './crons/DailyStat.js';
-import https from 'https';
-import fs from 'fs';
 
 const port = process.env.PORT;
 const app = express();
@@ -18,21 +16,13 @@ app.use(express.json()); //parse req.body to json
 app.use(cors({ corsOptions })); //whitelist the cors origin
 db(process.env.MONGO_DB_URI); //connect to mongoDB
 
-https
-  .createServer(
-    {
-      key: fs.readFileSync("key.pem"),
-      cert: fs.readFileSync("cert.pem"),
-    },
-    app
-  )
-  .listen(port, () => {
-    console.log(`Secure server is running on port ${port}`);
-  });
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 
 
 app.get("/", (req, res) => {
-  res.end("Welcome to the Sentimo's secure server!");
+    res.end("Welcome to the Sentimo's server!");
 })
 
 //setup routes 
