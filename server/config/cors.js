57fe0139005ee8f85913a25a892cb97c138a27aa
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-06-21 09:04:11 +0530

    config

--- server/config/cors.js
+++ server/config/cors.js
@@ -1,4 +1,4 @@
-const whitelist = ['http://localhost:3000', 'http://localhost:3001'];
+const whitelist = ['http://localhost:3000', 'http://localhost:3001', 'https://sentimo.surge.sh', 'http://sentimo.surge.sh/'];
 
 export const corsOptions = {
     origin: (origin, callback) => {
