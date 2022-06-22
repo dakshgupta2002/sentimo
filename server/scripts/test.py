ef1c672b2d3f9441f17753f015497f0063e3b599
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-06-22 10:57:09 +0530

    test script on new server

---
+++ server/scripts/test.py
@@ -0,0 +1,14 @@
+import sys
+import urllib.parse 
+import nltk
+nltk.download('omw-1.4')
+import text2emotion as te
+
+def emotion_output(text):
+    dcde = urllib.parse.unquote(text)
+    emotion_dict = te.get_emotion(dcde)
+
+    return emotion_dict
+
+
+print(emotion_output("Hello%20Everyone%20Iamvery%20sad%20sad%20sad%20sad"))
