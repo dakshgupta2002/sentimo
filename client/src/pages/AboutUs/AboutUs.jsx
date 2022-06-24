becf7a856181bf5f08c7ccfbcdbe470c59fac71e
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-06-24 19:53:05 +0530

    isbar ki pitayi

--- client/src/pages/AboutUs/AboutUs.jsx
+++ client/src/pages/AboutUs/AboutUs.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { ProfileCard } from "./../../elements";
 import { Sidebar } from "./../../components";
-import data from './aboutUsdata.js';
+import { data } from './aboutUsData.js';
 
 import "./AboutUs.css";
 
-const ishwarImg = require("./../../assets/avatars/ishwar_avatar.png");
-const dakshImg = require("./../../assets/avatars/daksh_avatar.png");
-const shirazImg = require("./../../assets/avatars/shiraz_avatar.png");
+import dakshImg from '../../assets/avatars/ishwar_avatar.png'
+import ishwarImg from '../../assets/avatars/ishwar_avatar.png'
+import shirazImg from '../../assets/avatars/ishwar_avatar.png'
 
 export default function AboutUs() {
   return (
@@ -17,34 +17,34 @@ export default function AboutUs() {
       <div className="profileCardParent">
         <div className="profileCard">
           <ProfileCard
-            name={data.daksh.name}
-            whatWork={data.daksh.whatWork}
-            whoAreYou={data.daksh.whoAreYou}
-            content={data.daksh.content}
-            socialLinks={data.daksh.socialLink}
-            profileImg={data.daksh.img}
+            name={data?.daksh?.name}
+            whatWork={data?.daksh?.whatWork}
+            whoAreYou={data?.daksh?.whoAreYou}
+            content={data?.daksh?.content}
+            socialLinks={data?.daksh?.socialLink}
+            profileImg={dakshImg}
           />
         </div>
 
         <div className="profileCard" style={{ backgroundColor: "#DFF6FF" }}>
         <ProfileCard
-            name={data.ishwar.name}
-            whatWork={data.ishwar.whatWork}
-            whoAreYou={data.ishwar.whoAreYou}
-            content={data.ishwar.content}
-            socialLinks={data.ishwar.socialLink}
-            profileImg={data.ishwar.img}
+            name={data?.ishwar?.name}
+            whatWork={data?.ishwar?.whatWork}
+            whoAreYou={data?.ishwar?.whoAreYou}
+            content={data?.ishwar?.content}
+            socialLinks={data?.ishwar?.socialLink}
+            profileImg={ishwarImg}
           />
         </div>
 
         <div className="profileCard">
         <ProfileCard
-            name={data.shiraz.name}
-            whatWork={data.daksh.whatWork}
-            whoAreYou={data.shiraz.whoAreYou}
-            content={data.shiraz.content}
-            socialLinks={data.shiraz.socialLink}
-            profileImg={data.shiraz.img}
+            name={data?.shiraz?.name}
+            whatWork={data?.daksh?.whatWork}
+            whoAreYou={data?.shiraz?.whoAreYou}
+            content={data?.shiraz?.content}
+            socialLinks={data?.shiraz?.socialLink}
+            profileImg={shirazImg}
           />
         </div>
       </div>
---
+++ client/src/pages/AboutUs/aboutUsData.js
@@ -0,0 +1,12 @@
+export const data = {
+
+        daksh:{
+            
+        },
+        ishwar:{
+            
+        },
+        shiraz:{
+            
+        }
+}
