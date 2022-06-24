0fcec84c93b44065f7c27568efada1b63768159a
Ishwarendra Jha <ishwarendra27@gmail.com>
2022-06-24 19:49:59 +0530

    png added

---
+++ client/src/pages/AboutUs/aboutUsdata.js
@@ -0,0 +1,30 @@
+const data = {
+  daksh: {
+    name: "Daksh Gupta",
+    whatWork: "whatWork",
+    whoAreYou: "whoAreYou",
+    content: "content",
+    socialLinks: ["#", "#", "#", "#"],
+    img: require("./../../assets/avatars/daksh_avatar.png")
+  },
+
+  ishwar: {
+    name: "Ishwarendra Jha",
+    whatWork: "whatWork",
+    whoAreYou: "whoAreYou",
+    content: "content",
+    socialLinks: ["#", "#", "#", "#"],
+    img: require("./../../assets/avatars/ishwar_avatar.png")
+  },
+
+  shiraz: {
+    name: "Shiraz Mangat",
+    whatWork: "whatWork",
+    whoAreYou: "whoAreYou",
+    content: "content",
+    socialLinks: ["#", "#", "#", "#"],
+    img: require("./../../assets/avatars/shiraz_avatar.png")
+  },
+};
+
+export default data;
