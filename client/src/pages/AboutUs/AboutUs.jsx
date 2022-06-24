d204d9c2d88c7d3fb321ad8fed0912086804aca0
Ishwarendra Jha <ishwarendra27@gmail.com>
2022-06-24 15:14:12 +0530

    barebone of profileCards ready

--- client/src/pages/AboutUs/AboutUs.jsx
+++ client/src/pages/AboutUs/AboutUs.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import {Sidebar} from './../../components';
+import React from "react";
+import { ProfileCard } from "./../../elements";
+import { Sidebar } from "./../../components";
 
 export default function AboutUs() {
   return (
     //make about us card in components and make 3 of them here
-    <div className='aboutUsBody'>
+    <div className="aboutUsBody">
       <Sidebar />
+      <div className="card">
+        <ProfileCard />
+      </div>
     </div>
-  )
+  );
 }
