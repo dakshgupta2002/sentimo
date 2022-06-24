1c6aa6a1668cbfc41d872dea6a3d5564e73528c5
Ishwarendra Jha <ishwarendra27@gmail.com>
2022-06-24 17:33:50 +0530

    pfp added in card

--- client/src/elements/ProfileCard/ProfileCard.jsx
+++ client/src/elements/ProfileCard/ProfileCard.jsx
@@ -4,41 +4,47 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import TwitterIcon from "@mui/icons-material/Twitter";
 
+import "./ProfileCard.css";
+
 export default function ProfileCard({
   name = "Name",
   whatWork = "whatWork",
   whoAreYou = "whoAreYou",
   content = "content",
-  socialLinks = ["#", "#", "#", "#"], /*Mail, Linkedin, Twitter, Github */
+  socialLinks = ["#", "#", "#", "#"] /*Mail, Linkedin, Twitter, Github */,
+  profileImg,
 }) {
   return (
     <div className="profileCardContainer">
       <div className="personData">
-        <h1 className="large-text">{name}.</h1>
-        <h3>{whatWork}</h3>
-        <h4>{whoAreYou}</h4>
-        <p>{content}</p>
-        <div className="profileCardSocial">
 
+        <h1 className="personName">{name}.</h1>
+        <h3 className="personWhatWork">{whatWork}</h3>
+        <h2 className="personWhoAreYou">{whoAreYou}</h2>
+        <p className="person3Content">{content}</p>
+
+        <div className="profileCardSocial">
           <a href={socialLinks[0]}>
-            <MailOutlinedIcon />
+            <MailOutlinedIcon sx={{ color: "black", fontSize: "40px" }} />
           </a>
 
           <a href={socialLinks[1]}>
-            <LinkedInIcon />
+            <LinkedInIcon sx={{ color: "black", fontSize: "40px" }} />
           </a>
 
           <a href={socialLinks[2]}>
-            <TwitterIcon />
+            <TwitterIcon sx={{ color: "black", fontSize: "40px" }} />
           </a>
 
           <a href={socialLinks[3]}>
-            <GitHubIcon />
+            <GitHubIcon sx={{ color: "black", fontSize: "40px" }} />
           </a>
         </div>
       </div>
 
-      <div className="pfp-container"></div>
+      <div className="pfpContainer">
+        <img className="avatars" src={profileImg} alt="" />
+      </div>
     </div>
   );
 }
--- client/src/pages/AboutUs/AboutUs.jsx
+++ client/src/pages/AboutUs/AboutUs.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import { ProfileCard } from "./../../elements";
 import { Sidebar } from "./../../components";
 
+import './AboutUs.css';
+
+const ishwarImg = require("./../../assets/avatars/ishwar_avatar.png");
+
 export default function AboutUs() {
   return (
     //make about us card in components and make 3 of them here
     <div className="aboutUsBody">
       <Sidebar />
-      <div className="card">
-        <ProfileCard />
+      <div className="profileCard">
+        <ProfileCard profileImg={ishwarImg}
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni officia esse mollitia, tempore eveniet delectus nisi sit, laborum molestias dolorum voluptate ipsum. Voluptatum illum nobis blanditiis ratione magni laudantium ad."/>
       </div>
     </div>
   );
