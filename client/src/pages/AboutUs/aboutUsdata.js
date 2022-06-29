ee3586eb3cf6ad3e40008887b1cd2768e633c0f0
Shiraz Mangat <78081835+shiraz-30@users.noreply.github.com>
2022-06-29 18:19:37 +0530

    AboutUs Data added

--- client/src/pages/AboutUs/aboutUsdata.js
+++ client/src/pages/AboutUs/aboutUsdata.js
@@ -2,7 +2,7 @@ export const data = {
   daksh: {
     name: "Daksh Gupta",
     whatWork: "Full Stack Developer",
-    whoAreYou: "Pursuing B.Tech. CSE from PEC",
+    whoAreYou: "Pursuing B.Tech. CSE from PEC, Chd",
     content: "I am MERN developer with special interest in data modelling for more efficiency. I am a growth orientated individual who believes in learning at every stage of life and overcoming challenges as they come! Single handedly created and hosted the complete backend of Sentimo with Node.js and AWS EC2 respectively.\nAlso performed supportive and managerial roles for Sentimo.",
     socialLinks: ["#", "#", "#", "#"],
     img: require("./../../assets/avatars/daksh_avatar.png")
@@ -19,10 +19,10 @@ export const data = {
 
   shiraz: {
     name: "Shiraz Mangat",
-    whatWork: "whatWork",
-    whoAreYou: "whoAreYou",
-    content: "content",
-    socialLinks: ["#", "#", "#", "#"],
+    whatWork: "Data Scientist",
+    whoAreYou: "Pursuing B.Tech. CSE from PEC, Chd",
+    content: "I am a Machine Learning and Data Science enthusiast. I am an individual who wants to inculcate my learnings to incorporate efficient and useful end results. I handled the ML portion & wrote Python scripts for emotion analysis in Sentimo. Also supported the designing part & performed Team Management.",
+    socialLinks: ["smangat1435@gmail.com", "https://www.linkedin.com/in/shiraz-mangat-33b4ba220/", "https://www.instagram.com/mangatshiraz/", "https://github.com/shiraz-30"],
     img: require("./../../assets/avatars/shiraz_avatar.png")
   },
 };
