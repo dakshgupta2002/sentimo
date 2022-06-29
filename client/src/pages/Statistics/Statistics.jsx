a3a6d6a27fbbe837107029e900b73f3cc3151f21
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-06-29 23:36:36 +0530

    formatting statistics data for graphs

--- client/src/pages/Statistics/Statistics.jsx
+++ client/src/pages/Statistics/Statistics.jsx
@@ -1,34 +1,51 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import { Sidebar } from "../../components";
 import { useDate } from "../../utils/hooks/useDate";
 import { fetchStats } from "../../utils/api/stats";
 import { Tab, Tabs } from "@mui/material";
 import { TabPanel } from "../../elements/TabPanel";
-
+// import { VictoryGraph}
 export default function Statistics() {
   const { date } = useDate();
   const [tab, setTab] = useState(0);
+  const numOfDays = [7, 30, 365];
+  const [emotions, setEmotions] = useState([]);
   const [pieChartData, setPieChartData] = useState([]);
-  const numOfDays = [7, 30, 365]
+  const [lineData, setLineData] = useState([]);
+
   const handleChange = (event, newValue) => {
     setTab(newValue);
   };
 
-  //recommendation is major here
+  // fetch the statistics created from past days
   useEffect(() => {
     const getStats = async () => {
       const res = await fetchStats(date, numOfDays[tab]);
-      console.log(res);
       if (res?.response?.status === 201 || res?.response?.status === 200) {
-        console.log(res?.data?.emotion);
+        //clear and update the emotions array
+        const updatedEmotion = [];
+        res?.data?.map((e) => {
+          updatedEmotion.push([e.date, e.emotion]);
+          return e;
+        });
+        setEmotions(updatedEmotion);
+        console.log(updatedEmotion[0])
       } else {
         console.log(res?.data?.msg);
       }
-    }
-
+    };
     getStats();
   }, [date, tab]);
+
+  useEffect( () => {
+    const refreshLineData = () => {
+
+    }
+
+
+    refreshLineData();
+  }, [emotions]) // update the data required when new emotions are created
 
   return (
     <div>
@@ -38,7 +55,20 @@ export default function Statistics() {
         <Tab label="Last Month" />
         <Tab label="Last Year" />
       </Tabs>
-
+      {emotions?.length===0 ? 
+        <h1>No emotions fetched... </h1> :
+        <div>
+          {emotions.map((emote, i) => {
+            return (<div key={i}>
+              <h2>{emote[0]}</h2>
+              <h6>Happy, Sad, Surprise, Angry, Fear</h6>
+              <h5>{emote[1].Happy}, {emote[1].Sad} {emote[1].Surprise}, {emote[1].Angry}, {emote[1].Fear}</h5>
+              <hr/>
+            </div>
+            )
+          })}
+        </div>
+      }
     </div>
   );
 }
