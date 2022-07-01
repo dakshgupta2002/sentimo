6561bf689b6ad7da40d609f350ea804c7049aa55
Ishwarendra Jha <ishwarendra27@gmail.com>
2022-07-01 13:27:40 +0530

    profile page skeleton

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Box } from "@mui/material";
+import { Box, TextField } from "@mui/material";
 import { Sidebar } from "../../components";
 import { fetchUserProfile } from "../../utils/api/user";
 import { useLoading } from "../../utils/hooks/useLoading";
+import { Avatar, IconButton, Input } from "@mui/material";
+
+import "./Profile.css";
 
 export default function Profile() {
   const [username, setUsername] = useState(null);
@@ -57,12 +60,144 @@ export default function Profile() {
   }, []);
 
   return (
-    <Box>
+    <div className="ProfileRoot">
       <Sidebar />
-      <h3>hi</h3>{" "}
-      <h1>
-        {firstName} {lastName}{" "}
-      </h1>
-    </Box>
+      <div className="profileFormContainer">
+        <div className="avatarContainer">
+          <Input accept="image/*" type="file" />
+          <label htmlFor="contained-button-file">
+            <IconButton>
+              <Avatar
+                img={image}
+                style={{
+                  margin: "10px",
+                  width: "60px",
+                  height: "60px",
+                }}
+              >
+                {firstName ? firstName[0] : ""}
+                {lastName ? lastName[0] : ""}
+              </Avatar>
+            </IconButton>
+          </label>
+        </div>
+
+        <div className="profileData mv">
+          <div className="mv mh">
+            <TextField
+              label="Username"
+              variant="outlined"
+              type="text"
+              InputProps={{
+                readOnly: true,
+              }}
+              fullWidth
+              // defaultValue={`${firstName}`} // doesn't work need to check
+              value={`${username}`}
+            />
+          </div>
+
+          <div className="mv mh">
+            <TextField
+              label="Email"
+              variant="outlined"
+              type="text"
+              InputProps={{
+                readOnly: true,
+              }}
+              fullWidth
+              // defaultValue={`${firstName}`} // doesn't work need to check
+              value={email ? email : "Not yet set"}
+            />
+          </div>
+          
+          <div className="mv mh">
+            <TextField
+              label="First Name"
+              variant="outlined"
+              type="text"
+              InputProps={{
+                readOnly: true,
+              }}
+              fullWidth
+              // defaultValue={`${firstName}`} // doesn't work need to check
+              value={firstName ? firstName : "Fetching..."}
+            />
+          </div>
+
+          <div className="mv mh">
+            <TextField
+              label="Last Name"
+              variant="outlined"
+              type="text"
+              InputProps={{
+                readOnly: true,
+              }}
+              fullWidth
+              // defaultValue={`${firstName}`} // doesn't work need to check
+              value={lastName ? lastName : "Fetching..."}
+            />
+          </div>
+              
+          <div className="mv mh">
+            <TextField
+              label="Phone Number"
+              variant="outlined"
+              type="text"
+              InputProps={{
+                readOnly: true,
+              }}
+              fullWidth
+              // defaultValue={`${firstName}`} // doesn't work need to check
+              value={phone ? phone : "Not yet set"}
+            />
+          </div>
+
+          <div className="mv mh">
+            <TextField
+              label="Gender"
+              variant="outlined"
+              type="text"
+              InputProps={{
+                readOnly: true,
+              }}
+              fullWidth
+              // defaultValue={`${firstName}`} // doesn't work need to check
+              value={gender ? gender : "Not yet set"}
+            />
+          </div>
+          
+          {/* Age from DOB maibi */}
+          <div className="mv mh">
+            <TextField
+              label="Age"
+              variant="outlined"
+              type="number"
+              InputProps={{
+                readOnly: true,
+              }}
+              fullWidth
+              // defaultValue={`${firstName}`} // doesn't work need to check
+              value={age ? age : "Not yet set"}
+            />
+          </div>
+
+          <div className="mv mh">
+            <TextField
+              label="Address"
+              variant="outlined"
+              type="number"
+              InputProps={{
+                readOnly: true,
+              }}
+              fullWidth
+              // defaultValue={`${firstName}`} // doesn't work need to check
+              value={age ? age : "Not yet set"}
+            />
+          </div>
+
+        </div>
+      </div>
+    </div>
   );
 }
