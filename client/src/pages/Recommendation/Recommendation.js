8eacf6d57e4371c7cce456cbff1ac4d29c67a05e
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-07-15 19:42:20 +0530

    Lazy Fetching movies, 20 at a time
    Co-authored-by: ishwarendra ishwarendra27@gmail.com

--- client/src/pages/Recommendation/Recommendation.js
+++ client/src/pages/Recommendation/Recommendation.js
@@ -11,22 +11,37 @@ import { MovieCard } from "../../elements";
 
 export default function Recommendation() {
   const [page, setPage] = useState(1);
-  const [movies, setMovies] = useState();
+  const [movies, setMovies] = useState([]);
   const [genreNames, setGenreNames] = useState({});
   const [emotion, setEmotion] = useState([0, 0, 0, 0, 0]);
-  //Happy Sad Angry Surprise Fear
   const [userGenre, setUserGenre] = useState()
-  const [genreModalOpen, setGenreModalOpen] = useState(true);
-
+  const [genreModalOpen, setGenreModalOpen] = useState(false);
+  const [lazyFetch, setLazyFetch] = useState(0);
   const { setLoading, setError, LoadingScreen } = useLoading();
   const { date } = useDate();
 
-  const discoverUrl = `https://api.themoviedb.org/3/discover/movie?api_key=4d3bc810d8690d3649ca4f41960de9b4&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_watch_monetization_types=flatrate`;
   const genreUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=4d3bc810d8690d3649ca4f41960de9b4&language=en-US`;
 
+  const getMoviesOnPage = async (page) => {
+    const res = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=4d3bc810d8690d3649ca4f41960de9b4&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_watch_monetization_types=flatrate`);
+    const body = await res.json();
+    return body.results;
+  }
+
+  window.addEventListener('scroll', (e) => {
+    if (
+      document.documentElement.scrollTop +
+      document.documentElement.clientHeight ===
+      document.documentElement.scrollHeight) {
+      setLazyFetch(lazyFetch + 1);
+    }
+    console.log("Reached end of the movies...")
+  })
 
   useEffect(() => {
     const getStats = async () => {
+      setLoading(true);
+      setError("Finding the best results");
       //get the emotion stats of last month
       const res = await fetchStats(date, 30);
       if (res?.response?.status === 201 || res?.response?.status === 200) {
@@ -44,12 +59,8 @@ export default function Recommendation() {
       } else {
         toast.error("Something went wrong.")
       }
+      setLoading(false);
     };
-
-    const mapStatToGenre = () => {
-      const genreSelected = MapGenre(emotion);
-      setUserGenre(genreSelected)
-    }
 
     //maps the genre ID to genre name as used by TMDB
     const getGenreNames = async () => {
@@ -62,40 +73,44 @@ export default function Recommendation() {
       setGenreNames(genreObj);
     };
 
-
-    setLoading(true);
-    setError("Finding the best results");
-
     getGenreNames();
     getStats();
-    mapStatToGenre();
-
-    setLoading(false);
   }, []);
+
+  useEffect(() => {
+    const genreSelected = MapGenre(emotion);
+    setUserGenre(genreSelected)
+  }, [emotion])
 
   useEffect(() => {
     //filter the movies with required genre ID for user
     const getMovies = async () => {
       let fetchedMovies = [];
-      while (fetchedMovies.length < 100){
+      let currentPage = page;
+      while (fetchedMovies.length < 20) {
         //keep searching for more movies
-        const res = await fetch(discoverUrl);
-        const body = await res.json();
-
+        const res = await getMoviesOnPage(currentPage);
+        fetchedMovies.push(...res)
+        currentPage += 1;
       }
+      //get next 20 movies and push to original array
+      console.log(fetchedMovies);
+      setMovies([...movies, ...fetchedMovies]);
+      setPage(currentPage);
     };
     getMovies();
-  }, [userGenre])
+  }, [lazyFetch]) //fetch 20 more movies every time lazy fetch changes
+
   return (
     <div className="reccomendationContain">
       <Sidebar />
       <LoadingScreen />
-      <ModalContainer isOpen={genreModalOpen} close={() => setGenreModalOpen(false)} style={{backgroundColor: '#2A2323'}}>
-    <img src={`https://images.unsplash.com/photo-1535446937720-e4cad0145efe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHBvc3RlciUyMG1vdmllfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60`} alt="" style={{height: 'fit-content'}}/>
+      <ModalContainer isOpen={genreModalOpen} close={() => setGenreModalOpen(false)} style={{ backgroundColor: '#2A2323' }}>
+        <img src={`https://images.unsplash.com/photo-1535446937720-e4cad0145efe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHBvc3RlciUyMG1vdmllfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60`} alt="" style={{ height: 'fit-content' }} />
       </ModalContainer>
 
       <div className="movieCardContainer">
-        {movies?.results?.map((movie, i) => {
+        {movies?.map((movie, i) => {
           return (
             <div className="movieCard" key={i}>
 
